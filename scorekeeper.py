1d33ccf0999a70b9c37793299bdcdb117efea13b
Dylan Whichard <dylan@whichard.com>
2015-08-28 17:42:58 -0400

    Rewrite for pyalexa api

--- scorekeeper.py
+++ scorekeeper.py
@@ -1,6 +1,7 @@
 import threading
 import requests
 from requests_oauthlib import OAuth1Session
+import pyalexa
 import flask
 import json
 import sign
@@ -19,6 +20,8 @@ TWITTER_APP_KEY_SECRET = keys["api_secret"]
 TWITTER_ACCESS_TOKEN = keys["access_token"]
 TWITTER_ACCESS_TOKEN_SECRET = keys["token_secret"]
 
+APP_ID = keys["app_id"]
+
 twitter = OAuth1Session(
     TWITTER_APP_KEY,
     client_secret=TWITTER_APP_KEY_SECRET,
@@ -27,6 +30,8 @@ twitter = OAuth1Session(
 )
 
 api = flask.Flask(__name__)
+
+skill = pyalexa.Skill(app_id=APP_ID)
 
 SIGN = sign.Sign("192.168.4.105", 8800)
 
@@ -54,12 +59,14 @@ def add_point(person):
 def points_for(person):
     return get_points().get(person.lower(), 0)
 
+def trim_reason(reason):
+    if reason and reason.lower().startswith("for "):
+        reason = reason[4:]
+    return reason
+
 def do_tweet(person, reason):
-    if reason:
-        if reason.lower().startswith("for "):
-            reason = reason[4:]
     r = twitter.post("https://api.twitter.com/1.1/statuses/update.json",
-                      data={"status": "Crazy point to {}: {}".format(person.title(), reason)})
+                      data={"status": "Crazy point to {}: {}".format(person.title(), trim_reason(reason))})
     print("Status:", r.text)
     print("Person", person, "\nReason", reason)
 
@@ -75,177 +82,64 @@ def _display_thread():
 def do_display():
     threading.Thread(target=_display_thread).start()
 
-@api.route('/', methods=['POST', 'GET'])
-def alexa():
-    data = flask.request.data
-    if data:
-        data = json.loads(data.decode('UTF-8'))
+@skill.launch
+def launch(request):
+    descs = ["{} has {} point{}".format(k, v, '' if v == 1 else 's') for (k, v) in
+             sorted(get_points().items(), key=lambda n:n[1], reverse=True) if v]
+
+    pretty_scores = "There are no scores yet."
+    if len(descs) == 1:
+        pretty_scores = ';'.join(descs)
+    elif len(descs) > 1:
+        pretty_scores = ';'.join(descs[:-1]) + '; and ' + descs[-1]
+
+    do_display()
+
+    return request.response(end=True, speech="Here is the Hackafay score breakdown: " + pretty_scores)
+
+@skill.end
+def end(request):
+    return request.response(end=True)
+
+@skill.intent("Point", "ConfirmPerson", "GiveReason")
+def main(request):
+    # Save each slot if it's 
+    if request.intent.slots.get("Person"):
+        request.save_slots("Person")
+    elif not request.session["Person"]:
+        return request.response("Who should I give a point to?")
+
+    if request.intent.slots.get("Reason"):
+        request.save_slots("Reason")
+    elif not request.session["Reason"]:
+        return request.response("What's the reason?")
+
+    return request.response("Should I give {} a point for {}?".format(
+        request.intent.slots["Person"],
+        trim_reason(request.intent.slots["Reason"])))
+
+@skill.intent("ConfirmPoint")
+def confirm_point(request):
+    if request.intent.slots.get("Confirmed").lower() in (
+            "yes", "yeah", "ok", "okay", "yep", "yup"):
+
+        person = request.session["Person"]
+        reason = request.session["Reason"]
+        num_points = points_for(person)
+
+        do_tweet(person, reason)
+
+        return request.response("{} now has {} point{}!".format(person, num_points, "" if num_points == 1 else "s"), end=True)
     else:
-        raise ValueError("Invalid arguments; no data")
+        return request.response(end=True)
 
-    with open('/tmp/alexa_test', 'a+') as f:
-        print("Data  : " + json.dumps(data, indent=2, separators=(',', ': ')) + '\n')
-
-    request = data["request"]
-    req_type = request["type"]
-    session_id = data["session"]["sessionId"]
-
-    attrs = {"person": None, "reason": None}
-    attrs.update(data["session"].get("attributes", {}))
-
-    if req_type == "LaunchRequest":
-        descs = ["{} has {} point{}".format(k, v, '' if v == 1 else 's') for (k, v) in
-                 sorted(get_points().items(), key=lambda n:n[1], reverse=True) if v]
-        pretty_scores = "There are no scores yet."
-        if len(descs) == 1:
-            pretty_scores = ';'.join(descs)
-        elif len(descs) > 1:
-            pretty_scores = ';'.join(descs[:-1]) + '; and ' + descs[-1]
-        resp = dict(BASE_RESPONSE)
-        resp["response"] = {
-            "outputSpeech": {
-                "type": "PlainText",
-                "text": "Here is the Hackafe score breakdown. {}".format(pretty_scores)
-            },
-            "shouldEndSession": True
-        }
-        do_display()
-        return flask.jsonify(resp)
-    elif req_type == "SessionEndedRequest":
-        resp = dict(BASE_RESPONSE)
-        resp["response"] = {
-            "shouldEndSession": True
-        }
-        return flask.jsonify(resp)
-    elif req_type == "IntentRequest":
-        # TODO validate timestamp
-        if "intent" in request:
-            intent = request["intent"]
-            name = intent['name']
-            slots = intent['slots']
-
-            if name == "Point" or name == "ConfirmPerson":
-                for slot in slots.values():
-                    slot_name = slot["name"]
-                    slot_val = slot.get("value", None)
-
-                    if slot_name == "Person":
-                        attrs["person"] = slot_val
-                    elif slot_name == "Reason":
-                        attrs["reason"] = slot_val
-
-                if attrs["reason"] and attrs["person"]:
-                    resp = dict(BASE_RESPONSE)
-                    resp["response"] = {
-                        "outputSpeech": {
-                            "type": "PlainText",
-                            "text": "Should I give {} a point for {}?".format(
-                                attrs["person"], attrs["reason"])
-                        },
-                        "shouldEndSession": False
-                    }
-                    resp["sessionAttributes"] = attrs
-                    return flask.jsonify(resp)
-                elif not attrs["person"]:
-                    print("Didn't get a name, reprompting")
-                    resp = dict(BASE_RESPONSE)
-                    resp["response"] = {
-                            "outputSpeech": {
-                                "type": "PlainText",
-                                "text": "Who should I give a point to?"
-                            },
-                        "shouldEndSession": False
-                    }
-                    resp["sessionAttributes"] = attrs
-                    return flask.jsonify(resp)
-                elif not attrs["reason"]:
-                    print("Prompting for a reason")
-                    resp = dict(BASE_RESPONSE)
-                    resp["response"] = {
-                        "outputSpeech": {
-                            "type": "PlainText",
-                            "text": "What's the reason?"
-                        },
-                        "shouldEndSession": False
-                    }
-                    resp["sessionAttributes"] = attrs
-                    return flask.jsonify(resp)
-            elif name == "GiveReason":
-                for slot in slots.values():
-                    slot_name = slot["name"]
-                    slot_val = slot.get("value", None)
-
-                    if slot_name == "Reason":
-                        attrs["reason"] = slot_val
-                if attrs["reason"] and attrs["person"]:
-                    resp = dict(BASE_RESPONSE)
-                    resp["response"] = {
-                        "outputSpeech": {
-                            "type": "PlainText",
-                            "text": "Should I give {} a point for {}?".format(
-                                attrs["person"], attrs["reason"])
-                        },
-                        "shouldEndSession": False
-                    }
-                    resp["sessionAttributes"] = attrs
-                    return flask.jsonify(resp)
-                elif not attrs["person"]:
-                    print("Didn't get a name, reprompting")
-                    resp = dict(BASE_RESPONSE)
-                    resp["response"] = {
-                            "outputSpeech": {
-                                "type": "PlainText",
-                                "text": "Who should I give a point to?"
-                            },
-                        "shouldEndSession": False
-                    }
-                    resp["sessionAttributes"] = attrs
-                    return flask.jsonify(resp)
-                elif not attrs["reason"]:
-                    print("Prompting for a reason")
-                    resp = dict(BASE_RESPONSE)
-                    resp["response"] = {
-                        "outputSpeech": {
-                            "type": "PlainText",
-                            "text": "What's the reason?"
-                        },
-                        "shouldEndSession": False
-                    }
-                    resp["sessionAttributes"] = attrs
-                    return flask.jsonify(resp)
-            elif name == "ConfirmPoint":
-                for slot in slots.values():
-                    slot_name = slot["name"]
-                    slot_val = slot.get("value", None)
-                    confirmed = False
-
-                    if slot_name == "Confirmed":
-                        confirmed = (slot_val.lower() == "yes")
-
-                    if confirmed:
-                        add_point(attrs["person"])
-                        num_points = points_for(attrs["person"])
-                        resp = dict(BASE_RESPONSE)
-                        resp["response"] = {
-                            "outputSpeech": {
-                                "type": "PlainText",
-                                "text": "{} now has {} point{}!".format(attrs["person"], num_points, "" if num_points == 1 else "s")
-                            },
-                        "shouldEndSession": True
-                        }
-                        do_tweet(attrs["person"],
-                                 attrs["reason"])
-                        resp["sessionAttributes"] = attrs
-                        return flask.jsonify(resp)
-                    else:
-                        resp = dict(BASE_RESPONSE)
-                        resp["response"] = {
-                            "shouldEndSession": True
-                        }
-                        resp["sessionAttributes"] = attrs
-                        return flask.jsonify(resp)
-
-    return {'args': repr(args), 'kwargs': repr(kwargs)}
+@skill.intent("Score")
+def score(request):
+    person = request.intent.slots.get("Person")
+    if person:
+        num_points = points_for(person)
+        return request.response("{} now has {} point{}!".format(person, num_points, "" if num_points == 1 else "s"), end=True)
+    return request.response("Whose score do you want to hear?")
 
 get_points()
 api.run('0.0.0.0', port=80, debug=True)
